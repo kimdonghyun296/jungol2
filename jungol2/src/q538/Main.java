ee2eff674778bb840705020e8e7a26100bfbce1d
user <user@192.168.219.106>
2025-01-18 01:47:35 +0900

    q538

--- jungol2/src/q538/Main.java
+++ jungol2/src/q538/Main.java
@@ -1,10 +1,30 @@
 package q538;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		while(true)
+		{
+			System.out.printf("number? ");
+			int num1=sc.nextInt();
+			
+			if(num1>0)
+			{
+				System.out.println("positive integer");
+			}
+			else if(num1<0)
+			{
+				System.out.println("negative number");
+			}
+			else
+			{
+				sc.close();
+				break;
+			}
+		}
 
 	}
-
 }
