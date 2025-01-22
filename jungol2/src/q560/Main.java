fe6109e946f69c879269bed3618a8db9ebbcd493
user <user@DESKTOP-EVAU1DU>
2025-01-22 09:19:58 +0900

    q560

---
+++ jungol2/src/q560/Main.java
@@ -0,0 +1,27 @@
+package q560;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+
+		int[] ary=new int[10];
+		for(int i=0;i<ary.length;i++)
+		{
+			ary[i]=sc.nextInt();
+		}
+		sc.close();
+		int min=ary[0];
+		for(int i=1;i<ary.length;i++)
+		{
+			if(ary[i]<=min)
+			{
+				min=ary[i];
+			}
+		}
+		System.out.println(min);
+	}
+
+}
